27108750b92112aedbdc59bb6839669535448186
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-02-13 15:08:08 +0000

    Replication of rest / web client result code 0 issue

--- gradle/src/main/java/com/example/demo/app/insights/UserController.java
+++ gradle/src/main/java/com/example/demo/app/insights/UserController.java
@@ -1,22 +1,52 @@
 package com.example.demo.app.insights;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
-    @Autowired
-    TelemetryClient telemetryClient;
+    private final TelemetryClient telemetryClient;
+    private final RestClient restClient = RestClient.create();
+    private final WebClient webClient = WebClient.create();
+    private final int serverPort;
+
+    public UserController(
+        final TelemetryClient telemetryClient,
+        @Value("${server.port}") final int serverPort
+    ) {
+        this.telemetryClient = telemetryClient;
+        this.serverPort = serverPort;
+    }
 
     @GetMapping("/greetings")
     public String greetings() {
+        return restClient.get()
+            .uri(String.format("http://localhost:%s/rest-greeting", serverPort))
+            .retrieve()
+            .body(String.class);
+    }
+
+    @GetMapping("/rest-greeting")
+    public Mono<@NonNull String> restGreeting() {
+        return webClient.get()
+            .uri(String.format("http://localhost:%s/web-greeting", serverPort))
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+
+    @GetMapping("/web-greeting")
+    public String webGreeting() {
         // send event
         telemetryClient.trackEvent("URI /greeting is triggered");
 
-        throw new NullPointerException();
+        return "Hello World!";
     }
 }
